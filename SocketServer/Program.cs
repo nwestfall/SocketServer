516faa36e512e3c5fb39ad740a4ab009f2b2f150
Nathan Westfall <nathan@fistbumpstudios.com>
2017-05-05 14:29:33 -0400

    initial commit

---
+++ SocketServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SocketServer server = new SocketServer();
+
+            Console.WriteLine("Welcome to the Socket Server Tester");
+            Console.WriteLine();
+
+            while(!server.VerifyIP())
+            {
+                Console.Write("Enter Socket IP (without port): ");
+                server.IP = Console.ReadLine();
+            }
+
+            while (server.Port == 0)
+            {
+                Console.Write("Enter Socket Port: ");
+                var portStr = Console.ReadLine();
+                int portNum;
+                if (int.TryParse(portStr, out portNum))
+                    server.Port = portNum;
+            }
+
+            Console.Write("Ready to start? (Y/N) ");
+            var key = Console.ReadLine();
+            if(key.StartsWith("y", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.WriteLine("Starting Socket Server!  Stop sending data by clicking any key in the console");
+
+                server.Start();
+
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            return; //QUIT
+        }
+    }
+}
---
+++ SocketServer/SocketServer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    public class SocketServer
+    {
+        public string IP { get; set; }
+        public int Port { get; set; }
+        private IPAddress Address;
+        private IPEndPoint Endpoint;
+        private Socket Socket;
+        private CancellationTokenSource CT;
+
+        public SocketServer() { }
+
+        public bool VerifyIP()
+        {
+            return IPAddress.TryParse(IP, out Address);
+        }
+
+        public async void Start()
+        {
+            CT = new CancellationTokenSource();
+            CT.Token.Register(() =>
+            {
+                Socket.Shutdown(SocketShutdown.Both);
+            });
+
+            try
+            {
+                Endpoint = new IPEndPoint(Address, Port);
+
+                if(Endpoint.Address.IsIPv6LinkLocal) //Support for IPv6
+                    Socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+                else
+                    Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                Console.WriteLine($"Socket setup with Address: {Endpoint.Address.ToString()}");
+
+                Socket.Bind(Endpoint);
+                Socket.Listen(10);
+
+                await Task.Run(() =>
+                {
+                    while (!CT.IsCancellationRequested)
+                    {
+                        Console.WriteLine("Waiting for a connection");
+
+                        Socket handler = Socket.Accept();
+
+                        Console.Write("Connection Recevied:" + handler.RemoteEndPoint.ToString());
+
+                        while (!CT.IsCancellationRequested)
+                        {
+                            string data = null;
+
+                            bool errorRec = false;
+
+                            while (!CT.IsCancellationRequested)
+                            {
+                                try
+                                {
+                                    byte[] bytes = new byte[1024];
+                                    int rec = handler.Receive(bytes);
+                                    if(rec != 0)
+                                        Console.WriteLine($"{rec} bytes received");
+                                    data += Encoding.ASCII.GetString(bytes, 0, rec);
+                                    if (rec != 1024 && rec != 0)
+                                        break;
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine("And error occured while receiving data: " + ex.StackTrace);
+                                    errorRec = true;
+                                    break;
+                                }
+                            }
+
+                            if (errorRec)
+                            {
+                                handler.Shutdown(SocketShutdown.Both);
+                                break;
+                            }
+
+                            Console.WriteLine($"Text Received: {data}");
+                        }
+                    }
+
+                    Socket.Shutdown(SocketShutdown.Both);
+                }, CT.Token);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Something went wrong: " + ex.StackTrace);
+            }
+        }
+        
+        public void Stop()
+        {
+            CT.Cancel();
+        }
+    }
+}
